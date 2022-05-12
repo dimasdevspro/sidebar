836725ddbd23378ff29267308346ab5b69e5f991
dimasdevspro <dimasdevspro@gmail.com>
2022-05-12 16:26:43 -0300

    developing...

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const inputSearch = document.getElementById('search')
 
 const sidebar = {
     moving(){
         document
         .querySelector('.navbar')
-        .classList.toggle('nav_extended')
-    inputSearch.style.width = "300px"
+        .classList.toggle('nav_extended');
+        document
+        .querySelector('.menu_logo')
+        .classList.toggle('menu_logo_at_right');
+        document
+        .querySelector('.logo')
+        .classList.toggle('logo_visible');
+        document
+        .querySelector('.search')
+        .classList.toggle('search_extended');
+        document
+        .querySelector('.menubuttons')
+        .classList.toggle('pvisible');
+        document
+        .querySelector('.logout')
+        .classList.toggle('logout_extended');
+        document
+        .querySelector('.logout_avatar')
+        .classList.toggle('logout_avatar_visible')
             }
             
 }
